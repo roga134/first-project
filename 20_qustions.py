2e8e044c61052b980ad91979d740b69d8fc81c77
roga134 <mohammad.mahdy.gamer@gmail.com>
2022-01-27 14:51:40 -0500

    Create 20_qustions.py
    I write some code for stile

---
+++ 20_qustions.py
@@ -0,0 +1,90 @@
+questions_list = [ 'is it alive?' , 'is it man?' , 'is it woman?' , 'is it car?' , 'is it city?' , 'is it fruit' ,    ]
+proper_noun_9 = [       ]
+
+
+#this print just mean rule of 20 questions 
+print('******************** welcom to 20 questions ********************')
+print('in this game my AI chooses one word and you can guess it in 20 questions ')
+print('rule of this game : you can just ask 20 questions , AI just should say yes or no , you can just start questions with is it and can it and end it with (?)')
+print('ok , lets start')
+print('now you can ask questions')
+ask_questions = input()
+
+# I choos 9 proper noun and first 3 is for mahdy and secend 3 for shervin and third 3 is for kiarash
+# mahdy
+def one_proper_noun():
+
+
+def two_proper_noun():
+
+
+
+def three_proper_noun():
+
+
+
+#shervin 
+def four_proper_noun():
+	
+
+
+def five_proper_noun():
+
+
+
+def six_proper_noun():
+	
+
+
+
+#kiarash
+def seven_proper_noun():
+	
+
+
+def eight_proper_noun():
+
+
+
+def nine_proper_noun():
+
+
+
+# this def divide mahdy and kiarash and shervin def to 3 part
+def mahdy_part():
+	one_proper_noun()
+	two_proper_noun()
+	three_proper_noun()
+
+
+def shervin_part():
+	four_proper_noun()
+	five_proper_noun()
+	six_proper_noun()
+
+
+def kiarash_part():
+	seven_proper_noun()
+	eight_proper_noun()
+	nine_proper_noun()
+
+
+# this def is main and call all of def 
+def main():
+	mahdy_part()
+	shervin_part()
+	kiarash_part()
+
+
+# this is for calling main def
+main()
+
+
+
+
+
+
+
+	
+
+
