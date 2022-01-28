e504ba74879759d6b8c0cf79a94fda6cb142a2ec
roga134 <mohammad.mahdy.gamer@gmail.com>
2022-01-27 21:07:41 -0500

    Update 20_qustions.py

--- 20_qustions.py
+++ 20_qustions.py
@@ -10,7 +10,6 @@ questions_list = [ 'is it alive?' , 'is it man?' , 'is it woman?' , 'is it car?'
 proper_noun_9 = [ 'cat'  , 'book' , 'dress' , 'guitar' , 'giraffe'  , 'keyboard'  , 'Bill Gates' , 'university' , 'photography' ]
 
 
-
 #this part choos one word random from proper_noun_9
 flag = True
 if flag == True :
@@ -18,7 +17,8 @@ if flag == True :
 	for i in random_word :
 		flag = False
 
-# this part will Conversion  questions list to string and use it in defs
+
+# this part will Conversion to string
 flag2 = True
 if flag2 == True :
 	for j in questions_list :
@@ -27,58 +27,81 @@ if flag2 == True :
 #this print just mean rule of 20 questions 
 print('******************** welcom to 20 questions ********************')
 print('in this game my AI chooses one word and you can guess it in 20 questions ')
-print('rule of this game : you can just ask 20 questions , AI just should say yes or no , you can just start questions with is it and can it and end it with (?)')
+print('rule of this game : you can just ask 20 questions , AI just should say yes or no ')
 print('ok , lets start')
 print('now you can ask questions')
-ask_questions = input()
+ask_questions = input().replace('is it ' , '').replace(' is it ' , '').replace('is it' , '').replace('?' , '').replace(' ?' , '').replace(' ? ' , '')
 
+
+# this part can say good night and ... to you if you want to go 
+def zone_time_part():
+    date_now = datetime.datetime.now()
+    time_now = float(date_now.strftime("%H"))
+    if time_now >= 5 and time_now <= 10 :
+        print('good morning')
+    elif time_now >= 11 and time_now <= 16 :
+        print('good noon')
+    elif time_now >= 17 and time_now <= 21 :
+        print('good after noon')
+    else :
+        print('good night')
 
 
 # I choos 9 proper noun and first 3 is for mahdy and secend 3 for shervin and third 3 is for kiarash
 # mahdy
-def one_proper_noun():
+def one_proper_noun():   # its cat
+	count = 0 
+	while :
+		count = count + 1
+		if count == 20 :
+			peint('ok you lose it was cat' + zone_time_part())
+		elif ask_questions == 'alive' :
+			print('yes')
+		elif ask_questions == 'animals' or ask_questions == 'animal' :
+			print('yes')
+		elif ask_questions == ''
 
 
 
-def two_proper_noun():
+#def two_proper_noun():
 
 
 
 
 
-def three_proper_noun():
+#def three_proper_noun():
 	
 
 
 #shervin 
-def four_proper_noun():
+#def four_proper_noun():
 	
 
 	
 
-def five_proper_noun():
+#def five_proper_noun():
 	
 
 
 
-def six_proper_noun():
+#def six_proper_noun():
 	
 	
 
 
 #kiarash
-def seven_proper_noun():
+#def seven_proper_noun():
 	
 
 	
 
 
-def eight_proper_noun():
+#def eight_proper_noun():
 	
 
 
 
-def nine_proper_noun():
+#def nine_proper_noun():
 	
 
 
