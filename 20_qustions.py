60fa0c9d4b7d8bfa2df147b2a100e8914e5e0f0c
roga134 <mohammad.mahdy.gamer@gmail.com>
2022-01-27 21:37:41 -0500

    Update 20_qustions.py

--- 20_qustions.py
+++ 20_qustions.py
@@ -2,14 +2,16 @@
 yes_part = ['yes', "Yes", "ya", 'Ya', 'yeap', 'Yeap', 'yeah', 'Yeah', 'ok', 'Ok', 'no Im ok', 'no I am ok','I can continue', 'please continue', 'continue', 'I know', 'I know ', 'of course ', 'of course','Of course ', 'Of course', 'yea', 'Yea', 'some', 'lot', 'know', 'maby', 'yes,I do.', 'yes, I do','yes I do', 'I do']
 #this code will import random for AI
 import random
+#this code will import time libery for AI
 import datetime
 
 
 
 # this part is 2 lists for using in AI 
-questions_list = ['Are there twelve characters?','Is it eleven letters?','Is it ten letters?','Is not it professional?','Are there eight letters?','Are there seven letters?','Are there six letters?','Are there five letters?','Are there four letters?','Are they two letters','Are there three letters?','Is it pentagonal?','Is it hexagonal?','Is it a lozenge?','Is it a triangle?','Is it a rectangle?','Is it a square?','Is it pollen?','Can it fit in your hand?','Is it the background?','Do you crawl?','Does it fly?', 'is it big?','is it small ?','Does it fit in your pocket?','is it alive?' , 'is it man?' , 'is it woman?' , 'is it car?' , 'is it city?' , 'is it fruit' ,    ]
+questions_list = ['is it alive?' , 'is it man?' , 'is it woman?' , 'is it car?' , 'is it city?' , 'is it fruit' ,    ]
 proper_noun_9 = [ 'cat'  , 'book' , 'dress' , 'guitar' , 'giraffe'  , 'keyboard'  , 'Bill Gates' , 'university' , 'photography' ]
-questions_cat = ['alive' , 'animals' , 'animal' , 'feet' , '4 feet' , 'four feet' , 'wool' , 'short wool' , 'made sound' , 'make sound']
+questions_cat = ['alive' , 'animals' , 'animal' , 'feet' , '4 feet' , 'four feet' , 'wool' , 'short wool' , 'made sound' , 'make sound' , '3 letters' , 'three letters' , 'run' , 'walk' , '3 characters' , 'three characters']
+
 
 #this part choos one word random from proper_noun_9
 flag = True
@@ -19,20 +21,13 @@ if flag == True :
 		flag = False
 
 
-# this part will Conversion to string
-flag2 = True
-if flag2 == True :
-	for j in questions_list :
-		flag2 = False
-
 #this print just mean rule of 20 questions 
 print('******************** welcom to 20 questions ********************')
 print('in this game my AI chooses one word and you can guess it in 20 questions ')
 print('rule of this game : you can just ask 20 questions , AI just should say yes or no ')
 print('ok , lets start')
 print('now you can ask questions')
-ask_questions = input().replace('is it ' , '').replace(' is it ' , '').replace('is it' , '').replace('?' , '').replace(' ?' , '').replace(' ? ' , '').replace('does it have a' , '').replace('Does it have' , '').replace('does it have ' , '').replace('does it have a '),replace('Does it have a', '').replace('can it ' , '').replace('Can it').replace('can it', '')
-
+ask_questions = input().replace('is it ' , '').replace(' is it ' , '').replace('is it' , '').replace('?' , '').replace(' ?' , '').replace(' ? ' , '').replace('does it have a' , '').replace('Does it have' , '').replace('does it have ' , '').replace('does it have a '),replace('Does it have a', '').replace('can it ' , '').replace('Can it').replace('can it', '').replace('it is' , '').replace('it is a' , '').replace('its ' , '').replace('its a' , '').replace('its a ').replace('its')
 
 # this part can say good night and ... to you if you want to go 
 def zone_time_part():
@@ -55,9 +50,15 @@ def one_proper_noun():   # its cat
 	while 1 == 1:
 		count = count + 1
 		if count == 20 :
-			peint('ok you lose it was cat' + zone_time_part())
+			print('ok you lose it was cat' + zone_time_part())
 		elif ask_questions in questions_cat :
 			print('yes')
+		elif ask_questions == 'cat':
+			print('nice its correct')
+		else :
+			print('no')
+		ask_questions = input().replace('is it ' , '').replace(' is it ' , '').replace('is it' , '').replace('?' , '').replace(' ?' , '').replace(' ? ' , '').replace('does it have a' , '').replace('Does it have' , '').replace('does it have ' , '').replace('does it have a '),replace('Does it have a', '').replace('can it ' , '').replace('Can it').replace('can it', '').replace('it is' , '').replace('it is a' , '').replace('its ' , '').replace('its a' , '').replace('its a ').replace('its')
+
 
 
 
