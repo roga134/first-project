edffb8d0f5d1525e438d06228b07dd928ed7c635
roga134 <mohammad.mahdy.gamer@gmail.com>
2022-02-14 21:06:00 -0500

    Update myfirstprogram.cpp

--- first_project/myfirstprogram.cpp
+++ first_project/myfirstprogram.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 
 int main() {
+  char a = 65 , b = 66 , c = 67 , d = 68 ;
+  cout << a  ;
+  cout << b  ;
+  cout << c  ;
+  cout << d  ;
   int x , y;
   cout << "type your number \n" ;
   cin >> x ;
   cout << "type another number \n" ;
   cin >> y ;
+  ++y;
+  --x;
+  cout << y << x  <<"\n" ;
   cout << x + y << "\n" ;
   return  0;
-
-}
-int mmad() {
-
-  float f1 = 35e3;
-  double d1 = 12E4;
-  cout << f1 << "\n";
-  cout << d1;
-  return 0;
-
 }
