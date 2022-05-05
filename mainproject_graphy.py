d41c12627a1e5da31a0274cccbae70cbb45faa52
roga134 <97909060+roga134@users.noreply.github.com>
2022-05-05 15:34:18 -0400

    Update mainproject_graphy.py

--- mainproject_graphy.py
+++ mainproject_graphy.py
@@ -1,109 +1,207 @@
-# this part we use Presentation for all of part like hi part and ...
-# we using this part for feeling_part
 feeling_good = ['I am good', 'Im good', 'Im not bad', 'I am not bad', 'Im perfect', 'I am perfect', 'tnx', 'Im ok',
                 'I am ok', 'fine tnx', ' fine thank you', 'fine thanks', 'fine', 'I am fine', 'Im fine' , 'good' , 'fine' ]
-# we can use this part in all of the AI its all pusetive word
 yes_part = ['yes', "Yes", "ya", 'Ya', 'yeap', 'Yeap', 'yeah', 'Yeah', 'ok', 'Ok', 'no Im ok', 'no I am ok',
             'I can continue', 'please continue', 'continue', 'I know', 'I know ', 'of course ', 'of course',
             'Of course ', 'Of course', 'yea', 'Yea', 'some', 'lot', 'know', 'maby', 'yes,I do.', 'yes, I do',
             'yes I do', 'I do']
-# we use this part for first or hi_part
 hi_part_array = ["hi", "Hi", "hello", "Hello", "hey", "Hey" , 'hi ' , 'Hi ' , 'Hello ' , 'hello ' , 'Hey ' , 'hey ']
-# this part is for county and its best country in my idea
 good_country = ['America', 'Russia', 'Japan', 'Canada', 'Turkish', 'Netherlands', 'Europe']
-# this part using for
 good_weather = ['good', 'so so', 'it is not bad', 'its not bad', 'it isnt bad']
-# this code will import request libery
-from requests import *
-# this code will import date libery
-import datetime
 small_game2 = ['its small' , 'its small ' , 'it is small' , 'it is small ' , 'small' , 'small ' , 'Small' , 'Small ']
 True_game2 = ['its true' , 'its true ' , 'it is true ' , 'it is true' , 'true' , 'true ' , 'True' , 'True '] 
+
+
+
+from requests import *
+import datetime
 from tkinter import *
 import tkinter.font
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image
+import sys
+from time import sleep
+import os
+import time
+
+
+
 
 project_graphy = tkinter.Tk()
 project_graphy.title('*********AI*********')
+project_graphy.resizable(0,0)
 project_graphy.configure(background = '#F2B33D' )
-project_graphy.geometry('3000x1000')
-
-
-text_box1 = tk.Label(project_graphy , text = '**************Welcome to my AI*************' , bg = '#F2B33D' , fg = 'black' , font = ('Comic Sans MS' , 38 , 'bold'))
-text_box1.grid(row = 0 , column = 4   )
-
-
-text_box2 = tk.Label(project_graphy , text = 'Hi' , bg = '#F2B33D' , fg = 'black' ,  font = ('Comic Sans MS' , 24 , 'bold'))
-text_box2.grid(row = 1 , column = 4 )
-
-
-text_box3 = tk.Label(project_graphy, text="your text" , fg = 'black' , bg = '#F2B33D' , font = ('Comic Sans MS' , 27 , 'bold'))
-text_box3.grid(row = 2, column = 0 )
-
-
-input_box1 = Entry(project_graphy )
-input_box1.grid(row = 2, column = 1 )
-
-
-text_box4 = tk.Label(project_graphy, text = 'how are you?' , fg = "black" , bg = '#F2B33D' , font = ('Comic Sans MS' , 27 , 'bold'))
-
-
-text_box5 = tk.Label(project_graphy, text="your text" , fg = 'black' , bg = '#F2B33D' , font = ('Comic Sans MS' , 27 , 'bold'))
-
-
-input_box2 = Entry(project_graphy )
-
-
-text_box6 = tk.Label(project_graphy , text = 'tnx God' , fg = 'black' , bg = '#F2B33D' ,font = ('Comic Sans MS' , 33 , 'bold') )
-
-
-
-print(input_box1.get())
+project_graphy.geometry('1000x700')
 
 
 
 
-def new_text_world1():
-	text_box4.grid(row = 0 , column = 4)
-	text_box5.grid(row = 1, column = 0 )
-	input_box2.grid(row = 1, column = 1 )
-	button2.grid(row = 1, column = 3   )
-
-print(input_box2.get())
-
-def feeling() :
-	if input_box2.get() in feeling_good :
-		text_box6.grid(row = 0 , column= 4) 
-		text_box4.after(500 , text_box4.destroy())
-		text_box5.after(500 , text_box5.destroy())
-		input_box2.after(500 , input_box2.destroy())
-		button2.after(500 , button2.destroy())
-		
 
 
+	
 
 
-def delete_all1():
-	text_box1.after(500, text_box1.destroy())
-	text_box2.after(500, text_box2.destroy())
-	text_box3.after(500, text_box3.destroy())
-	button1.after(500, button1.destroy())
-	input_box1.after(500, input_box1.destroy())
-	new_text_world1()
+text_box_main = tk.Label(project_graphy, text="your text" , fg = 'black', bg = '#F2B33D' , font = ('Comic Sans MS' , 27 ))
+text_box_main.place(x=290 , y = 105)
+	
+if_input = StringVar()
+
+input_box_main = Entry(project_graphy , textvariable = if_input )
+input_box_main.place(x = 410 , y = 115)
+	
+user_pick = if_input.get()
+
+but_exit = Button(project_graphy , text = "Exit"  , font = ('Comic Sans MS' , 30 ) , fg = 'black' , bg = '#F2B33D' , command = good_night)
+but_exit.place(x = 450 , y = 200)
+	
+
+
+
+
+
+def good_night():
+	date_now = datetime.datetime.now()
+	time_now = float(date_now.strftime("%H"))
+	delete_all_text()
+	if time_now >= 5 and time_now <= 20 :
+		tx_end1 = Label(project_graphy , text = "have good day" , font = ('Comic Sans MS' , 30 ) , bg = "#F2B33D" , fg = "black").pack()
+	else :
+		Label(project_graphy , text = "good night" , font = ('Comic Sans MS' , 30 ) , bg = "#F2B33D" , fg = "black").pack()
+
+
+
+
+
+def delete_all_text():
+	print("34")
+	text_box1.after(0 , text_box1.destroy())
+	text_box2.after(0 , text_box2.destroy())
+	text_box4.after(0 , text_box4.destroy())
+	text_box6.after(0 , text_box6.destroy())
+	text_box7.after(0 , text_box7.destroy())
+	if_input.set("")
+	button1.after(0, button1.destroy())
+	button2.after(0, button2.destroy())
+	text_box_main.after(0 , text_box_main.destroy)
+	input_box_main.after(0 , input_box_main.destroy)
+	but_exit.after(0 , but_exit.destroy())
+
+
+
+
+
+# this part I make text box and input box
+def text_input_button_part():
+	#this part for get global the for using variable on all of project
+	global time_now
+	global text_box1
+	global text_box2
+	global text_box4
+	global text_box6
+	global text_box7
+	global text_box8
+	global text_box9
+	global text_box10
+	global text_box11
+	global text_box12
+	global button2
+	global button1
+	
+
+
+
+	text_box1 = tk.Label(project_graphy , text = '**************Welcome to my AI*************' ,bg = '#F2B33D'  , fg = 'black' , font = ( 'Comic Sans MS' , 33 ))
+	text_box1.pack()
+	text_box2 = tk.Label(project_graphy , text = 'Hi' , bg = '#F2B33D', fg = 'black' ,  font = ('Comic Sans MS' , 24 ))
+	text_box2.pack()
+	main_text()
+	
+
+
+	text_box4 = tk.Label(project_graphy, text = 'how are you?' , fg = "black" , bg = '#F2B33D' , font = ('Comic Sans MS' , 27 ))
+
+
+	
+
+	text_box6 = tk.Label(project_graphy , text = 'tnx God' , fg = 'black' , bg = '#F2B33D' ,font = ('Comic Sans MS' , 33 ) )
+
+
+	text_box7 = tk.Label(project_graphy, text = 'you get better' , fg = 'black' , bg = '#F2B33D' , font = ('Comic Sans MS' , 33 ))
+
+
+
+
+	button1 = Button(project_graphy , text = "if your typing is done click here"  , font = ('Comic Sans MS' , 27 ) , fg = 'black' , bg = '#F2B33D' , command = hi_part )
+	button1.place(x = 290 , y = 150)
+	
+
+
+	button2 = Button(project_graphy , text = "if your typing is done click here"  , font = ('Comic Sans MS' , 27 ) , fg = 'black' , bg = '#F2B33D' , command = feeling )
+
+
+
+	
+
+
+
 
 
 def hi_part() :
-	if input_box1.get() in hi_part_array :
+	user_pick = if_input.get()
+	print(user_pick)
+	if user_pick in hi_part_array :
 		delete_all1()
 	else :
-		exit()
+		good_night()
 
-button1 = Button(project_graphy , text = "if your typing is done click here"  , font = ('Comic Sans MS' , 27 , 'bold') , fg = 'black' , bg = '#F2B33D' , command = hi_part )
-button1.grid(row = 2, column = 2   )
 
-button2 = Button(project_graphy , text = "if your typing is done click here"  , font = ('Comic Sans MS' , 27 , 'bold') , fg = 'black' , bg = '#F2B33D' , command = feeling )
+def delete_all1():
+	text_box1.after(100, text_box1.destroy())
+	text_box2.after(100, text_box2.destroy())
+	button1.after(100, button1.destroy())
+	new_text_world1()
+
+def new_text_world1():
+	text_box4.pack()# how are you
+	main_text()
+	button2.place( x = 290 , y = 150)
+
+
+def feeling() :
+	user_pick = if_input.get()
+	if user_pick in feeling_good :
+		text_box4.after(0 , text_box4.destroy())
+		button2.after(0 , button2.destroy())
+		text_box6.pack() # tnx god
+		new_text_world2()
+	elif input_box2.get() not in feeling_good :
+		text_box4.after(0 , text_box4.destroy())
+		text_box5.after(0 , text_box5.destroy())
+		input_box2.after(0 , input_box2.destroy())
+		button2.after(0 , button2.destroy())
+		text_box7.pack()# you get better
+		new_text_world2()
+
+
+def new_text_world2():
+	button3.place(x = 290 , y = 150)
+
+
+
+
+
+
+
+text_input_button_part()
+
+
+
+	
+
+
+
+
+
 
 
 project_graphy.mainloop()
