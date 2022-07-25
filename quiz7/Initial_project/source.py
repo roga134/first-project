9b8fe51417d2e5e484254455b03f44a0f2353f94
roga134 <97909060+roga134@users.noreply.github.com>
2022-07-25 10:15:23 -0400

    Update source.py

--- quiz7/Initial_project/source.py
+++ quiz7/Initial_project/source.py
@@ -1,60 +1,43 @@
 def words_check(text):
-	cca = []
-	bb = {}
-	a = text.split()
-	for i in a :
-		p = i.casefold().capitalize()# part part of the text
-		asd = a.count(i)
-		if "!" in p :
-			count_g(p)
-			qe = p.count("!")
-			g = count - qe
-			if g == qe or g < qe :
-				q = p.replace("!" , "" )
-				cca.append(q)
-		elif "#" in p :
-			count_g(p)
-			qr = p.count("#")
-			g = count - qr
-			if g == qr or g < qr: 
-				e = p.replace("#" , "")
-				cca.append(e)
-		elif "_" in p :
-			count_g(p)
-			qqq = p.count("_")
-			g = count - qqq
-			if g == qqq or g < qqq :
-				ee = p.replace("_" , "").replace("?" , "")
-				cca.append(ee)
-		elif "?" in p :
-			count_g(p)
-			t = p.count("?")
-			g = count - t
-			if g == t or g < t :
-				ert = p.replace("?" , "")
-				cca.append(ert)
-		elif "'" in p  :
-			count_g(p)
-			t = p.count("'")
-			g = count - t
-			if g > t :
-				er = p.replace("'" , "")
-				cca.append(er)
-		else :
-			cca.append(p)
-	b = sorted(cca)
-	for j in b:
-		bb[j] = asd
-	print(bb)
-
-def count_g(c):
-	global count
+	list_good_word = [ "a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" ,"s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"] 
+	count_good_word = 0
+	count_bad_word = 0
+	dicth = {}
+	list_made_for_dic = []
+	list_wh_o_scored = text.split()# ['hEllO', 'My', 'FriEnDs!!!', 'thIS', 'is', 'A', 'tEsT', 'For', 'your', '#p#r#o#b#l#e#m']
 	count = 0
-	for w in c :
-		count += 1
+	for i in list_wh_o_scored :
+		number_count_word = list_wh_o_scored.count(i) # its count one word in list
+		if_alpha = i.isalpha() # its undrestan # , ! , _ and ...
+		if if_alpha == True :
+			correct_word = i.casefold().capitalize()
+			list_made_for_dic.append(correct_word) #Hello My This Is A Test For Your
+		else :
+			# i = FriEnDs!!! , #p#r#o#b#l#e#m
+			for j in i :
+				print(j)
+				smaller_of_i = j.lower()
+				count += 1
+				if count == (len(i)+ 1) :
+					count = 0 
+					if count_good_word >= count_bad_word  :
+						count_bad_word = 0
+						count_bad_word = 0
+						if smaller_of_i in list_good_word :
+							count_good_word += 1
+						else :
+							count_bad_word += 1
+				else :
+					if smaller_of_i in list_good_word :
+						count_good_word += 1
+					else :
+						count_bad_word += 1
 
 
+	list_made_for_dic_sc = sorted(list_made_for_dic)
+	for w in list_made_for_dic_sc :
+		dicth[w] = number_count_word
 
 
-words_check('''hEllO My FriEnDs!!! thIS is A tEsT For your #p#r#o#b#l#e#m''')#{'A': 1, 'For': 1, 'Friends': 1, 'Hello': 1, 'Is': 1, 'My': 1, 'Test': 1, 'This': 1, 'Your': 1}
+words_check('''hEllO My FriEnDs!!! thIS is A tEsT For your #p#r#o#b#l#e#m''') #{'A': 1, 'For': 1, 'Friends': 1, 'Hello': 1, 'Is': 1, 'My': 1, 'Test': 1, 'This': 1, 'Your': 1}
 #words_check('''HeLLLO_O My________________FRIEND HOW ARE YOUUUUU?___? I Don'T KNow Y_O_U_R_N_A_M_E yet !!!!!!!!''')#{'Are': 1, 'Dont': 1, 'Hellloo': 1, 'How': 1, 'I': 1, 'Know': 1, 'Yet': 1, 'Yourname': 1, 'Youuuuu': 1}
